c2ac2c0b17a41e93777543d8c98b8c37f4925c96
Jayhello <haluoluo@qq.com>
2017-03-19 15:40:38 +0800

    1.add operate about numpy

--- python_utils/machine_learn/perception/perception.py
+++ python_utils/machine_learn/perception/perception.py
@@ -1,5 +1,5 @@
 # _*_ coding:utf-8 _*_
-import numpy
+import numpy as np
 import sys
 from utils.FileUtil import get_line_lst
 
@@ -8,10 +8,57 @@ date_path = '../dataset/perception/dataset.txt'
 train_set = []
 
 
+class Perception(object):
+
+    def __init__(self, var_num):
+        self.w = np.random.randn(1, var_num)
+        self.b = 1
+        self.var_num = var_num
+        self.min_error_rate = 0.02
+
+    def get_current_para(self):
+        return self.w, self.b
+
+    def train(self, train_data, eta):
+        pass
+
+    def SGD(self, train_data, epoch, eta):
+
+        for i in xrange(epoch):
+            self.train(train_data, eta)
+
+            current_error_rate = self.get_error_rate(train_data)
+            print 'epoch {0} current_error_rate: {1}'.format(i+1, current_error_rate)
+            if current_error_rate <= self.min_error_rate:
+                break
+
+    def get_error_rate(self, validate_data):
+        return 0
+
+
 def generate_date():
     lst_data = get_line_lst(date_path)
 
+    lst_ret = []
+    for item in lst_data:
+        lst_ret.append(item.split())
 
+    return lst_ret
 
 if __name__ == '__main__':
-    pass
+    # generate_date()
+    var_num = 2
+    # p = Perception(var_num)
+    # print p.get_current_para()[0]
+
+    # arr1 = np.array([1, 2])
+    # arr2 = np.array([3, 4])
+    # print arr1 * arr2
+    # print np.dot(arr1, arr2.transpose())
+    tp = (1, 2, 3)
+    lst = [[1, 2], [3, 4, 5]]
+    lst = [[1, 2], [3, 4]]
+    print np.array(lst).shape
+    print np.array(lst)
+    print np.asarray(lst)
+    print np.asarray(tp)
--- python_utils/utils/FileUtil.py
+++ python_utils/utils/FileUtil.py
@@ -2,9 +2,15 @@
 
 
 def get_line_lst(filename):
+    """
+    return list of every line in file
+    :param filename the file to be readed
+        like '../dataset/dataset.txt':
+    :return like [['line1 1'], ['line2 2']]:
+    """
     lst = []
     with open(filename) as fp:
-        for line in enumerate(fp):
+        for line in fp:
             lst.append(line)
 
     return lst
