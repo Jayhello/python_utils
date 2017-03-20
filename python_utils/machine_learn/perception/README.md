c8ea217245ffa42fc202d0dff457c92d16493830
Jayhello <haluoluo@qq.com>
2017-03-20 20:07:52 +0800

    1.add changes about lst and db

--- python_utils/machine_learn/perception/README.md
+++ python_utils/machine_learn/perception/README.md
@@ -1,2 +1,16 @@
-# This Util is about how to realize Perception
+# one level topic
+* This is about first level
++ Every nice to meet you
+- just show non num column
 
+## two level topic
+1. this is num column
+2. I am row 2
+ * a blank at line head
+ + hybrid use
+* no whitespace
+
+#### four level topic
+1. ###### this nested usage
+2. ### this nested usage
+3. see the difference
--- python_utils/py_basic/tuple_operate.py
+++ python_utils/py_basic/tuple_operate.py
@@ -9,5 +9,25 @@ def list_2_tuple():
     tp = tuple(tuple(item) for item in lst)
     print tp
 
+
+def tuple_shift_left(tup, n):
+    """
+    shift tuple over by n indices
+    :param tup: like (1,2,3,4)
+    :param n: 1
+    :return: (2, 3, 4, 1)
+    """
+    if n < 0:
+        raise ValueError('n must be a positive integer')
+    if not tup or not n:
+        return tup
+    n %= len(tup)
+    return tup[n:] + tup[:n]
+
 if __name__ == '__main__':
-    list_2_tuple()
+    # list_2_tuple()
+    tp = (1, 2, 3, 4)
+    # print tuple_shift_left(tp, -1)
+    # (2, 3, 4, 1)
+    print tuple_shift_left(tp, 5)
+    # (2, 3, 4, 1)
