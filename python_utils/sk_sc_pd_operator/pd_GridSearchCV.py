a5b8376f38a8e7e79f44bf481946562340ae0f17
Jayhello <haluoluo@qq.com>
2018-01-01 20:38:41 +0800

    1. sklearn KFold 2. interleaving string

--- python_utils/sk_sc_pd_operator/pd_GridSearchCV.py
+++ python_utils/sk_sc_pd_operator/pd_GridSearchCV.py
@@ -1,9 +1,14 @@
 # coding:utf-8
 
+import numpy as np
 import pandas as pd
 from sklearn import svm, datasets
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 
 def test_grid_search_cv():
@@ -25,6 +30,43 @@ def test_grid_search_cv():
     print(classification_report(y_true=iris.target, y_pred=y_pred))
 
 
+def grid_search_cv_graph():
+    iris = datasets.load_digits()
+    X = iris.data
+    Y = iris.target
+
+    C_lst = [1, 10, 100, 1000]
+    gamma_lst = [0.125, 0.25, 0.5, 1, 2, 4]
+    gamma_lst = [1e-3, 1e-4]
+
+    parameters = {'C': C_lst, 'gamma': gamma_lst}
+
+    # parameters = {'kernel': ('linear', 'rbf'),
+    #               'C': C_lst, 'gamma': gamma_lst}
+
+    clf_ = svm.SVC()
+    clf = GridSearchCV(clf_, parameters, cv=2, n_jobs=-1)
+    clf.fit(X, Y)
+
+    print clf.best_params_
+    print clf.best_score_
+
+    print clf.cv_results_
+
+    # scores = [x[1] for x in clf.grid_scores_]
+    scores = clf.cv_results_['mean_test_score']
+    print scores
+    scores = np.array(scores).reshape(len(C_lst), len(gamma_lst))
+
+    for ind, i in enumerate(C_lst):
+        plt.plot(gamma_lst, scores[ind], label='C: ' + str(i))
+
+    plt.legend()
+    plt.xlabel('Gamma')
+    plt.ylabel('Mean score')
+    plt.show()
+
 if __name__ == '__main__':
-    test_grid_search_cv()
+    # test_grid_search_cv()
+    grid_search_cv_graph()
     pass
